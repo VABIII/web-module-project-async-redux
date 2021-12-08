32083ab2d72ad012123a8f66619f29f4315a360a
VABIII <tbengtson82@gmail.com>
2021-12-08 13:45:51 -0600

    axios call returning data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
+
+
+
 
 function App() {
-  return (
+    const [characters, setCharacters] = useState([])
+    console.log(characters)
+    useEffect(() => {
+        axios.get(` https://rickandmortyapi.com/api/character`)
+            .then(res => {
+                console.log(res.data.results)
+                setCharacters(res.data.results)
+
+            })
+
+            .catch(err => {
+                console.error(err)
+            })
+
+
+    }, [])
+
+
+
+    return (
     <div className="App">
-      Async Redux Project
+      <h1>You Son Of A Bitch, I'm In!</h1>
+        <h4>Async Redux Project</h4>
     </div>
   );
 }
